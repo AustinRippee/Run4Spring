c2bf88f1ed7c61a065aeed76f3b40c37d6fdf0f9
Austin Rippee <austinrippee@gmail.com>
2026-07-02 07:29:53 -0400

    Bug Fixes: - Removed self-referential dependency in pom.xml (was preventing the server from building) - Removed the @GetMapping("/") "Hello World" controller that was blocking index.html from loading - Spring Boot now correctly serves index.html as the welcome page
    Additions:
    - HTML pages

--- src/main/java/com/austinrippee/run4spring/paceconvert/PaceConvertService.java
+++ src/main/java/com/austinrippee/run4spring/paceconvert/PaceConvertService.java
@@ -28,7 +28,7 @@ public class PaceConvertService {
     }
 
     private double computeScore(String distance, double t) {
-        return switch (distance) {
+        return switch (distance.toLowerCase().trim()) {
             case "100m", "100" -> 290.52712 * (100.0 / t) - 1953.2266;
             case "150m", "150" -> 265.3031224 * (150.0 / t) - 1720.7734;
             case "200m", "200" -> 267.75893 * (200.0 / t) - 1703.6447;
@@ -39,30 +39,28 @@ public class PaceConvertService {
             case "1000m", "1000", "1,000", "1,000m", "1k", "1km" -> 313.6503268 * (1000.0 / t) - 1374.25166;
             case "1500m", "1500", "1,500", "1,500m", "1.5k", "1.5km" -> 320.6038 * (1500.0 / t) - 1314.0045;
             case "1600m", "1600", "1,600", "1,600m", "1.6k", "1.6km" -> 321.7731201 * (1600.0 / t) - 1306.325127;
-            case "1 Mile", "1Mile", "Mile", "One Mile", "1609", "1609m",
-                 "1 mile", "1mile", "1mi", "mile", "one mile", "One mile", "one Mile" ->
+            case "1 mile", "1mile", "mile", "one mile", "1609", "1609m", "1mi" ->
                     321.7731201 * (1609.34 / t) - 1306.285127;
             case "2000m", "2000", "2,000", "2,000m", "2k" -> 328.2988442 * (2000.0 / t) - 1303.430804;
             case "3000m", "3000", "3,000", "3,000m", "3k" -> 331.264214 * (3000.0 / t) - 1240.294895;
             case "3200m", "3200", "3,200", "3,200m" -> 333.4505158 * (3200.0 / t) - 1241.705275;
-            case "2 Mile", "2 Miles", "2Mile", "2Miles", "Two Miles", "Two Mile",
-                 "3218", "3218m", "2mi", "2MI", "2Mi" ->
+            case "2 mile", "2 miles", "2mile", "2miles", "two miles", "two mile", "3218", "3218m", "2mi" ->
                     333.4505158 * (2 * 1609.34 / t) - 1241.705275;
             case "5000m", "5,000m", "5000", "5,000", "5k", "5km" -> 342.8535 * (5000.0 / t) - 1234.1959;
             case "6000m", "6,000m", "6000", "6,000", "6k", "6km" -> 344.0777994 * (6000.0 / t) - 1217.284313;
             case "8000m", "8,000m", "8000", "8,000", "8k", "8km" -> 348.6258257 * (8000.0 / t) - 1192.426848;
             case "10000m", "10,000m", "10000", "10,000", "10k", "10km" -> 349.8535 * (10000.0 / t) - 1171.2847;
-            case "10 Miles", "10mi" -> 360.6890152 * (10 * 1609.34 / t) - 1164.451907;
-            case "HalfM", "Half Marathon", "HM", "Half", "13.1", "13.1mi", "13.1 Miles" ->
+            case "10 miles", "10mi" -> 360.6890152 * (10 * 1609.34 / t) - 1164.451907;
+            case "halfm", "half marathon", "hm", "half", "13.1", "13.1mi", "13.1 miles" ->
                     366.3739581 * ((42194.99 / 2) / t) - 1168.783894;
-            case "Marathon", "M", "Full Marathon", "Full", "Mara", "26.2", "26.2mi", "26.2 Miles" ->
+            case "marathon", "m", "full marathon", "full", "mara", "26.2", "26.2mi", "26.2 miles" ->
                     384.5408 * (42194.99 / t) - 1161.8021;
             default -> throw new IllegalArgumentException("Invalid race distance: " + distance);
         };
     }
 
     private String computeConvertedTime(String target, double score) {
-        double totalSecs = switch (target) {
+        double totalSecs = switch (target.toLowerCase().trim()) {
             case "100m", "100" -> 100.0 / (0.003439 * score + 6.72526);
             case "150m", "150" -> 150.0 / (0.003768768 * score + 6.486427968);
             case "200m", "200" -> 200.0 / (0.003734 * score + 6.36315);
@@ -76,8 +74,7 @@ public class PaceConvertService {
                     1500.0 / (0.003117 * score + 4.09988);
             case "1600m", "1600", "1,600", "1,600m", "1.6k", "1.6km" ->
                     1600.0 / (0.0031062 * score + 4.060811045);
-            case "1 Mile", "1Mile", "Mile", "One Mile", "1609", "1609m",
-                 "1 mile", "1mile", "1mi", "mile", "one mile", "One mile", "one Mile" ->
+            case "1 mile", "1mile", "mile", "one mile", "1609", "1609m", "1mi" ->
                     1609.34 / (0.0031062 * score + 4.060811045);
             case "2000m", "2000", "2,000", "2,000m", "2k" ->
                     2000.0 / (0.0030444 * score + 3.97139601);
@@ -85,8 +82,7 @@ public class PaceConvertService {
                     3000.0 / (0.0030147 * score + 3.74703401);
             case "3200m", "3200", "3,200", "3,200m" ->
                     3200.0 / (0.0029949 * score + 3.726724);
-            case "2 Mile", "2 Miles", "2Mile", "2Miles", "Two Miles", "Two Mile",
-                 "3218", "3218m", "2mi", "2MI", "2Mi" ->
+            case "2 mile", "2 miles", "2mile", "2miles", "two miles", "two mile", "3218", "3218m", "2mi" ->
                     (2 * 1609.34) / (0.0029949 * score + 3.726724);
             case "5000m", "5,000m", "5000", "5,000", "5k", "5km" ->
                     5000.0 / (0.0029129 * score + 3.602496);
@@ -96,11 +92,11 @@ public class PaceConvertService {
                     8000.0 / (0.0028672 * score + 3.4212486);
             case "10000m", "10,000m", "10000", "10,000", "10k", "10km" ->
                     10000.0 / (0.002857 * score + 3.348169);
-            case "10 Miles", "10mi" ->
+            case "10 miles", "10mi" ->
                     16093.4 / (0.0027716 * score + 3.2290278);
-            case "HalfM", "Half Marathon", "HM", "Half", "13.1", "13.1mi", "13.1 Miles" ->
+            case "halfm", "half marathon", "hm", "half", "13.1", "13.1mi", "13.1 miles" ->
                     (42194.99 / 2) / (0.0026921 * score + 3.098524);
-            case "Marathon", "M", "Full Marathon", "Full", "Mara", "26.2", "26.2mi", "26.2 Miles" ->
+            case "marathon", "m", "full marathon", "full", "mara", "26.2", "26.2mi", "26.2 miles" ->
                     42194.99 / (0.0026921 * score + 3.098524);
             default -> throw new IllegalArgumentException("Invalid target distance: " + target);
         };
