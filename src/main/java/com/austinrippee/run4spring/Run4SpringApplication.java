959b7f8e67c0132d235bc0aaf6d5fc06aed7ba2d
Austin Rippee <austinrippee@gmail.com>
2026-07-01 21:11:49 -0400

    Added dependencies. Added root mapping message

--- src/main/java/com/austinrippee/run4spring/Run4SpringApplication.java
+++ src/main/java/com/austinrippee/run4spring/Run4SpringApplication.java
@@ -2,9 +2,17 @@ package com.austinrippee.run4spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Run4SpringApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Run4SpringApplication.class, args);
